c3fd0a8b372c64254f76152d60d7b72097546a01
Pagend <pariy.illya@gmail.com>
2025-02-03 23:13:52 +0200

    added validation for the same keys

--- lib/redis_test/pairs.ex
+++ lib/redis_test/pairs.ex
@@ -1,26 +1,21 @@
 defmodule RedisTest.Pairs do
   def create_pair(db_conn, key, value) do
-    Redix.command(db_conn, ["SET", key, value])
+    case Redix.command(db_conn, ["SETNX", key, value]) do
+      {:ok, 1} -> {:ok, "OK"}
+      {:ok, 0} -> {:error, "Key is already in use"}
+      {:error, reason} -> {:error, reason}
+    end
   end
 
   def list_pairs(db_conn) do
-    case Redix.command(db_conn, ["KEYS", "*"]) do
-      {:ok, keys} when is_list(keys) and keys != [] ->
-        case Redix.command(db_conn, ["MGET" | keys]) do
-          {:ok, values} ->
-            Enum.map(Enum.zip(keys, values), fn {key, value} ->
-              %{"key" => key, "value" => value}
-            end)
-
-          {:error, reason} ->
-            {:error, {:fetch_values_failed, reason}}
-        end
-
-      {:ok, []} ->
-        []
-
-      {:error, reason} ->
-        {:error, {:fetch_keys_failed, reason}}
+    with {:ok, keys} when is_list(keys) and keys != [] <- Redix.command(db_conn, ["KEYS", "*"]),
+         {:ok, values} <- Redix.command(db_conn, ["MGET" | keys]) do
+      Enum.map(Enum.zip(keys, values), fn {key, value} ->
+        %{"key" => key, "value" => value}
+      end)
+    else
+      {:ok, []} -> []
+      {:error, reason} -> {:error, {:fetch_failed, reason}}
     end
   end
 
@@ -38,11 +33,20 @@ defmodule RedisTest.Pairs do
   end
 
   def update_pair(db_conn, old_key, new_key, new_value) do
-    with {:ok, _} <- delete_pair(db_conn, old_key),
-         {:ok, "OK"} <- create_pair(db_conn, new_key, new_value) do
-      {:ok, %{"key" => new_key, "value" => new_value}}
-    else
-      {:error, reason} -> {:error, reason}
+    case Redix.command(db_conn, ["EXISTS", new_key]) do
+      {:ok, 1} when old_key != new_key ->
+        {:error, "Key is already in use"}
+
+      {:ok, _} ->
+        with {:ok, _} <- delete_pair(db_conn, old_key),
+             {:ok, "OK"} <- create_pair(db_conn, new_key, new_value) do
+          {:ok, %{"key" => new_key, "value" => new_value}}
+        else
+          {:error, reason} -> {:error, reason}
+        end
+
+      {:error, reason} -> 
+        {:error, reason}
     end
   end
 
